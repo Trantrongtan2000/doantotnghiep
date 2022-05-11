12ce2c52a4b5c72993c14c5a364ea94322c2f425
Trantrongtan2000 <32.trantrongtan.92@gmail.com>
2022-05-11 15:29:24 +0700

    up thoai

--- testvat/testvat.py
+++ testvat/testvat.py
@@ -1,7 +1,48 @@
-import image
+import cv2
+import numpy as np
+# read image
+image = cv2.imread('mau3.jpg')
+img=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# get dimensions of image
+blurM = cv2.medianBlur(img, 5)
+edgeM = cv2.Canny(blurM, 100, 200)
 
-im=image.open('mau1.jpg')
+blurG = cv2.GaussianBlur(img, (9, 9), 0)
+edgeG = cv2.Canny(blurG, 100, 100)
 
-imbox=im.getbox()
-crop=im.crop(imbox)
-crop.save('anhcut.jpg')
+img=edgeG
+# height, width, number of channels in image
+height = img.shape[0]
+width = int(img.shape[1])
+
+print(height)
+print(width)
+
+#img[start row, end row, start col and col]
+crop=img[:,0:int(width/3)]
+crop2=img[:,int(width/3):int(width/3*2)]
+crop3=img[:,int(width/3*2):int(width)]
+'''a1=np.concatenate((crop,crop2,crop3),axis=0)
+cv2.imshow('',a1)
+cv2.waitKey(0)'''
+
+hist, bin=np.histogram(crop)
+print('')
+print(hist)
+print('')
+print(bin)
+a=hist[-1]
+hist, bin=np.histogram(crop2)
+print('')
+print(hist)
+print('')
+print(bin)
+b=hist[-1]
+hist, bin=np.histogram(crop3)
+print('')
+print(hist)
+print('')
+print(bin)
+c=hist[-1]
+print('')
+print('%s %s %s'%(a,b,c))
