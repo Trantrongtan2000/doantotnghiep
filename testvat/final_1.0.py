60b582ee3a0b21ec05cecfbc442559719821cd3c
Trantrongtan2000 <32.trantrongtan.92@gmail.com>
2022-05-17 18:12:42 +0700

    update 05172022 final.py

--- testvat/final_1.0.py
+++ testvat/final_1.0.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-image = cv2.imread('mau3.jpg')
+image = cv2.imread('mau5.jpg')
 
 #image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
 img=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -58,7 +58,7 @@ def xetnhommau(a,b,rh):
             else:
                 nm='A-'
     else:
-        if B == True:
+        if b == True:
             if rh== True:
                 nm='B+'
             else:
@@ -69,7 +69,61 @@ def xetnhommau(a,b,rh):
             else:
                 nm= 'O-'
     return print('Nhom mau: '+nm)
+
+check=[]
+demM=0
+demG=0
 AM,BM,RHM=catanh(edgeM)
 AG, BG,RHG=catanh(edgeG)
+fillM=catanh(edgeM)
+fillG=catanh(edgeG)
+if AM and BM and RHM and AG and BG and RHG > 200: #Xet tat ca duong
+  if AM and AG > 300 and abs(AM-AG) >10:
+      check.append(True)
+  else:
+      check.append(False)
+  if BM and BG > 300 and abs(BM-BG) >10:
+      check.append(True)
+  else:
+      check.append(False)
+  if RHM and RHG > 300 and abs(RHM-RHG) >10:
+      check.append(True)
+  else:
+      check.append(False)
 
-xetnhommau(True,False,False)
+else:
+    for t in fillM:
+        if t == 0:
+            demM += 1
+    for t in fillG:
+        if t == 0:
+            demG += 1
+    print(demM)
+    print(demG)
+    if demM == 1 and demG == 1:
+        for t in fillM:
+            for i in fillG:
+                if t == 0 and i == 0:
+                    check.append(False)
+                elif t > 100 and i > 100:
+                    check.append(True)
+    if demM != 0 and bool(AG and BG and RHG > 300) or demG != 0 and bool(AM and BM and RHM > 300):
+        if AM == 0 or AG == 0:
+            check.append(False)
+        else:
+            check.append(True)
+        if BM == 0 or BG == 0:
+            check.append(False)
+        else:
+            check.append(True)
+        if RHM == 0 or RHG == 0:
+            check.append(False)
+        else:
+            check.append(True)
+
+
+
+print(bool(AG and BG and RHG > 300))
+print(demM != 0)
+xetnhommau(check[0], check[1], check[2])
+#print(check)
